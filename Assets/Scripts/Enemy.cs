65217b1f3f189c5d31fac291d36750391e06e202
Hiroki Tanaka <tanaka.hiroki@gmail.com>
2015-01-08 16:01:05 +0900

    first commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown()
+	{
+		Destroy(gameObject);
+	}
+
+	void OnDestroy(){
+		GameObject go = GameObject.Find ("GameManager");
+		if (go != null) {
+			GameManager gm = go.GetComponent(typeof(GameManager)) as GameManager;
+			gm.AddScore(100);
+		}
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+	public UnityEngine.UI.Text timeText;
+	public UnityEngine.UI.Text scoreText;
+
+	int myScore = 0;
+	float remainingTime = 60.0f;
+
+	// Use this for initialization
+	void Start () {
+		MakeEnemys ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float deltaTime = Time.deltaTime;
+		remainingTime -= deltaTime;
+		timeText.text = ("" + remainingTime);
+
+		scoreText.text = ("score:" + myScore);
+	}
+
+	void MakeEnemys()
+	{
+		for (int i = 0; i < 10; i++) {
+			GameObject enemy = (GameObject)Instantiate(enemyPrefab);
+			Vector3 p = enemy.transform.position;
+			p.x = Random.Range(-2,2);
+			p.y = Random.Range(-4,4);
+			enemy.transform.position = p;
+		}
+	}
+
+	public void AddScore(int add)
+	{
+		if (remainingTime > 0) {
+			myScore += add;		
+		}
+	}
+}
